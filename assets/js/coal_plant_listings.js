40b6f450350e32ba6deb4cba654fadd9e68e00c4
Tzu-Chi Yen <junipertcy@gmail.com>
2018-02-05 16:51:20 +0800

    added more files

---
+++ assets/js/coal_plant_listings.js
@@ -0,0 +1,32 @@
+var plants = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [139.9736, 35.4534]
+            },
+            "properties": {
+                "name": "千葉袖ケ浦火力発電所1号機（仮)",
+                "capacity": "100.0 [万kW]",
+                "status": "アセスメント実施中",
+                "title": "Mapbox SF",
+                "icon": "harbor"
+            }
+        }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [139.982, 35.4744]
+            },
+            "properties": {
+                "name": "千葉袖ケ浦火力発電所2号機（仮)",
+                "capacity": "100.0 [万kW]",
+                "status": "アセスメント実施中",
+                'marker-color': '#3bb2d0',
+                'marker-size': 'large',
+                'marker-symbol': 'rocket'
+            }
+        }]
+};
---
+++ assets/js/map_operations.js
@@ -0,0 +1,43 @@
+function createPopUp(currentFeature) {
+  var popUps = document.getElementsByClassName('mapboxgl-popup');
+  // Check if there is already a popup on the map and if so, remove it
+  if (popUps[0]) popUps[0].remove();
+
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML('<h3>Plant</h3>' +
+      '<h4>' + currentFeature.properties.address + '</h4>')
+    .addTo(map);
+}
+
+function buildLocationList(data) {
+  // Iterate through the list of stores
+  for (i = 0; i < data.features.length; i++) {
+    var currentFeature = data.features[i];
+    // Shorten data.feature.properties to just `prop` so we're not
+    // writing this long form over and over again.
+    var prop = currentFeature.properties;
+    // Select the listing container in the HTML and append a div
+    // with the class 'item' for each store
+    var listings = document.getElementById('listings');
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.className = 'item';
+    listing.id = 'listing-' + i;
+
+    // Create a new link with the class 'title' for each store
+    // and fill it with the store address
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+    link.dataPosition = i;
+    link.innerHTML = prop.name;
+
+    // Create a new div with the class 'details' for each store
+    // and fill it with the city and phone number
+    var details = listing.appendChild(document.createElement('div'));
+    details.innerHTML = prop.capacity;
+    if (prop.status) {
+      details.innerHTML += ' &middot; ' + prop.status;
+    }
+  }
+}
