e99d15843f5251789156e0d5e2b37f823ef38efd
Tzu-Chi Yen <junipertcy@gmail.com>
2018-03-02 17:57:34 +0800

    play/pause function prototype done

--- assets/js/map_operations.js
+++ assets/js/map_operations.js
@@ -6,22 +6,24 @@ function flyToStore(currentFeature) {
 }
 
 function createPopUp(currentFeature) {
-    var id = currentFeature.properties.id.slice(2);
+    let id = currentFeature.properties.id.slice(2);
     console.log("hey ID = ", id);
     console.log("selected = ", selected_pollutant);
 
-
     omnivore.kml('dataset/japan' + id + '_' + selected_pollutant + '_concentration_monthly.kml').on('ready', function (d) {
-        var sourceName = selected_pollutant + '_' + id;
-        var layerName = sourceName + "_layer";
+        let sourceName = selected_pollutant + '_' + id;
+        let layerName = sourceName + "_layer";
+        _layerName = layerName;
+        _id = id;
+        _pollutant = selected_pollutant;
         ls = d.target.getLayers();
 
-        for (var i = 0; i < ls.length; ++i) {
+        for (let i = 0; i < ls.length; ++i) {
             try {
-                var dd = ls[i].feature;
-                var c_ = [];
+                let dd = ls[i].feature;
+                let c_ = [];
                 dd.geometry.coordinates[0].forEach(function (o) {
-                    var dropped = _.dropRight(o, 1);
+                    let dropped = _.dropRight(o, 1);
                     c_.push(dropped);
                 });
                 ls[i].feature.geometry.coordinates[0] = c_;
@@ -31,7 +33,7 @@ function createPopUp(currentFeature) {
                 }
             }
         }
-        var pre_data = _.map(ls, 'feature');
+        let pre_data = _.map(ls, 'feature');
 
         pre_data.map(function (d) {
             try {
@@ -53,6 +55,7 @@ function createPopUp(currentFeature) {
             return d;
         });
         console.log('pre_data = ', pre_data);
+
         map.addSource(sourceName, {
             'type': 'geojson',
             'data': {
@@ -60,16 +63,6 @@ function createPopUp(currentFeature) {
                 "features": _.map(ls, 'feature')
             }
         });
-        // try {s
-        //     map.removeLayer('pollution-polygons');
-        // } catch (e) {
-        //     console.log(e);
-        // }
-        // try {
-        //     map.removeSource('pollutions');s
-        // } catch (e) {
-        //     console.log(e);
-        // }
         map.addLayer({
             'id': layerName,
             'type': 'fill',
@@ -86,17 +79,18 @@ function createPopUp(currentFeature) {
         filterBy(0, layerName);  // January
 
         document.getElementById('slider').addEventListener('input', function (e) {
-            var month = parseInt(e.target.value);
+            let month = parseInt(e.target.value);
             filterBy(month, layerName);
         });
-        var colors = [];
-        for (i = 0; i < pre_data.length; i++) {
-            var layer = pre_data[i].properties.name;
-            var color;
 
-            var item = document.createElement('div');
-            var key = document.createElement('div');
-            var value = document.createElement('span');
+        let colors = [];
+        for (let i = 0; i < pre_data.length; i++) {
+            let layer = pre_data[i].properties.name;
+            let color;
+
+            let item = document.createElement('div');
+            let key = document.createElement('div');
+            let value = document.createElement('span');
 
             if (colors.length === 3) {
                 continue;
@@ -140,11 +134,11 @@ function createPopUp(currentFeature) {
 
     });
 
-    var popUps = document.getElementsByClassName('mapboxgl-popup');
+    let popUps = document.getElementsByClassName('mapboxgl-popup');
     // Check if there is already a popup on the map and if so, remove it
     if (popUps[0]) popUps[0].remove();
 
-    var popup = new mapboxgl.Popup({closeOnClick: true, closeButton: true})
+    let popup = new mapboxgl.Popup({closeOnClick: true, closeButton: true})
         .setLngLat(currentFeature.geometry.coordinates)
         .setHTML(
             // '<h4>' + currentFeature.properties.name + '</h4>' +
@@ -188,7 +182,7 @@ function createPopUp(currentFeature) {
         )
         .addTo(map);
 
-    var close_button = document.getElementsByClassName('mapboxgl-popup-close-button');
+    let close_button = document.getElementsByClassName('mapboxgl-popup-close-button');
     // close_button[0].className = "className"
     close_button[0].style['display'] = 'block';
     close_button[0].style['margin-top'] = '0px';
@@ -197,35 +191,36 @@ function createPopUp(currentFeature) {
     close_button[0].style['background-image'] = "url(data:image/svg+xml;base64,PHN2ZyB4bWxuczpzdmc9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMCAyMCIgdmVyc2lvbj0iMS4xIiBoZWlnaHQ9IjIwIiB3aWR0aD0iMjAiPg0KICA8cGF0aCBkPSJtNSA1IDAgMS41IDMuNSAzLjUtMy41IDMuNSAwIDEuNSAxLjUgMCAzLjUtMy41IDMuNSAzLjUgMS41IDAgMC0xLjUtMy41LTMuNSAzLjUtMy41IDAtMS41LTEuNSAwLTMuNSAzLjUtMy41LTMuNS0xLjUgMHoiIGZpbGw9IiMwMDAiLz4NCjwvc3ZnPg==)";
 
     popup.on('close', function (e) {
-        var activeItem = document.getElementsByClassName('active');
-        if (activeItem[0]) {
-            activeItem[0].classList.remove('active');
-            map.flyTo({
-                // center: [142.61871875040669, 38.13053360748921],
-                // zoom: 5
-            })
-        }
+        // let activeItem = document.getElementsByClassName('active');
+        // if (activeItem[0]) {
+        //     activeItem[0].classList.remove('active');
+        //     map.flyTo({
+        //         // center: [142.61871875040669, 38.13053360748921],
+        //         // zoom: 5
+        //     })
+        // }
 
     });
 }
 
 function buildLocationList(data) {
     // Iterate through the list of stores
-    for (i = 0; i < data.features.length; i++) {
-        var currentFeature = data.features[i];
+    for (let i = 0; i < data.features.length; i++) {
+        let currentFeature = data.features[i];
         // Shorten data.feature.properties to just `prop` so we're not
         // writing this long form over and over again.
-        var prop = currentFeature.properties;
+        let prop = currentFeature.properties;
+        console.log(prop);
         // Select the listing container in the HTML and append a div
         // with the class 'item' for each store
-        var listings = document.getElementById('listings');
-        var listing = listings.appendChild(document.createElement('div'));
+        let listings = document.getElementById('listings');
+        let listing = listings.appendChild(document.createElement('div'));
         listing.className = 'item';
-        listing.id = 'listing-' + i;
+        listing.id = 'listing-' + prop.id.slice(2);
 
         // Create a new link with the class 'title' for each store
         // and fill it with the store address
-        var link = listing.appendChild(document.createElement('a'));
+        let link = listing.appendChild(document.createElement('a'));
         link.href = '#';
         link.className = 'title';
         link.dataPosition = i;
@@ -233,25 +228,25 @@ function buildLocationList(data) {
 
         // Create a new div with the class 'details' for each store
         // and fill it with the city and phone number
-        var details = listing.appendChild(document.createElement('div'));
+        let details = listing.appendChild(document.createElement('div'));
         details.innerHTML = prop.capacity;
         if (prop.status) {
             details.innerHTML += ' &middot; ' + prop.status;
         }
         // Add an event listener for the links in the sidebar listing
         link.addEventListener('click', function (e) {
-            // Update the currentFeature to the store associated with the clicked link
-            var clickedListing = data.features[this.dataPosition];
-            // 1. Fly to the point associated with the clicked link
+            let clickedListing = data.features[this.dataPosition];
             flyToStore(clickedListing);
-            // 2. Close all other popups and display popup for clicked store
             createPopUp(clickedListing);
-            // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-            var activeItem = document.getElementsByClassName('active');
+            let activeItem = document.getElementsByClassName('active');
             if (activeItem[0]) {
                 activeItem[0].classList.remove('active');
             }
             this.parentNode.classList.add('active');
+
+
+
+
         });
     }
 
@@ -262,18 +257,17 @@ function kml2hex(kml_str) {
     if (kml_str[0] === '-') {
         kml_str = kml_str.substr(1)
     }
-    // var alpha = parseInt(kml_str.slice(0, 2), 16);
-    // var blue = parseInt(kml_str.slice(2, 4).toUpperCase(), 16);
-    // var green = parseInt(kml_str.slice(4, 6).toUpperCase(), 16);
-    // var red = parseInt(kml_str.slice(6, 8).toUpperCase(), 16);
+    // let alpha = parseInt(kml_str.slice(0, 2), 16);
+    // let blue = parseInt(kml_str.slice(2, 4).toUpperCase(), 16);
+    // let green = parseInt(kml_str.slice(4, 6).toUpperCase(), 16);
+    // let red = parseInt(kml_str.slice(6, 8).toUpperCase(), 16);
     // return [red, green, blue, alpha];
     // rgba = converter.kmlToRgba(kml_str);
     // return 'rgba(' + rgba.r + ',' + rgba.g + ',' + rgba.b + ',' + rgba.a + ')';
-    return;  // TODO or DELETE
 }
 
 function getColor(level) {
-    var color;
+    let color;
     if (level === '0.05 - 0.1') {
         color = '#5990e2'
     } else if (level === '0.1 - 0.2') {
